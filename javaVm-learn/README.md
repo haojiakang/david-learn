0fc64a9b07c8b353bdf6b42e0cdc33aeef160d76
Jackie <946392690@qq.com>
2016-07-23 01:11:44 +0800

    'nothing'

---
+++ javaVm-learn/README.md
@@ -0,0 +1,1 @@
+This is my JavaVM learn project.
---
+++ javaVm-learn/src/main/java/com.david.learn/HeapOOM.java
@@ -0,0 +1,23 @@
+package com.david.learn;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by David on 2016/7/23.
+ * VM args: -Xms20m -Xmx20m -XX:+HeapDumpOnOutOfMemoryError
+ */
+public class HeapOOM {
+
+    static class OOMObject {
+
+    }
+
+    public static void main(String[] args) {
+        List<OOMObject> list = new ArrayList<>();
+        while (true) {
+            list.add(new OOMObject());
+        }
+    }
+
+}
