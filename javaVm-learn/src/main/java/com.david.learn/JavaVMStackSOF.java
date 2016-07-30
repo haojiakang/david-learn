4e105051f77e354c05fffa9f0a35a35d10ab9d7e
Jackie <946392690@qq.com>
2016-07-30 09:38:32 +0800

    added some class of JVM

---
+++ javaVm-learn/src/main/java/com.david.learn/JavaVMStackSOF.java
@@ -0,0 +1,26 @@
+package com.david.learn;
+
+/**
+ * Created by Jackie on 2016/7/23
+ * VM args: -Xss128k
+ */
+public class JavaVMStackSOF {
+
+    private int stackLength = 1;
+
+    public void stackLeak() {
+        stackLength++;
+        stackLeak();
+    }
+
+    public static void main(String[] args) {
+        JavaVMStackSOF oom = new JavaVMStackSOF();
+        try {
+            oom.stackLeak();
+        } catch (Throwable throwable) {
+            System.out.println("stack length: " + oom.stackLength);
+            throw throwable;
+        }
+    }
+
+}
