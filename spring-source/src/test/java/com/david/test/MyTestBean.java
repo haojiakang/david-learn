f7e5f295922ce1f62fcddda1379b127be796e4a6
haojk <haojk@fxiaoke.com>
2017-04-12 10:36:50 +0800

    added beanFactoryTest

---
+++ spring-source/src/test/java/com/david/test/MyTestBean.java
@@ -0,0 +1,16 @@
+package com.david.test;
+
+/**
+ * Created by haojk on 4/10/17.
+ */
+public class MyTestBean {
+    private String testStr = "testStr";
+
+    public String getTestStr() {
+        return testStr;
+    }
+
+    public void setTestStr(String testStr) {
+        this.testStr = testStr;
+    }
+}
