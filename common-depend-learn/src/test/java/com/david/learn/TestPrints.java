43270315dded25fcfdfb2a6bae3ff185b83938bc
jiakang <jiakang@staff.sina.com.cn>
2017-05-19 16:55:16 +0800

    nothing

---
+++ common-depend-learn/src/test/java/com/david/learn/TestPrints.java
@@ -0,0 +1,15 @@
+package com.david.learn;
+
+import com.david.common.util.Prints;
+import org.junit.Test;
+
+/**
+ * Created by jiakang on 2017/5/19.
+ */
+public class TestPrints {
+
+    @Test
+    public void testPrint() {
+        Prints.println("Hello world");
+    }
+}
